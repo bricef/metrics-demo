8741ed48223c6979b431f1c8475b8806b333a89f
Brice Fernandes <brice@weave.works>
2017-09-21 18:13:06 +0100

    Create API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # metrics-demo
+
+[![Docker Repository on Quay](https://quay.io/repository/brice/metrics-demo/status "Docker Repository on Quay")](https://quay.io/repository/brice/metrics-demo)
+
 Quick and dirty prometheus metrics demo
--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const catalogue = require("./catalogue.json")
+
+const app = express()
 
 var public_path = path.join(__dirname, 'public');
 
-app.use('/static', express.static(public_path))
+// Parse incoming json
+app.use(bodyParser.json())
+app.set('json spaces', 2);
 
+// Serve static files from public dir
+app.use('/public', express.static(public_path))
+
+app.post("/api/purchase", function(req, res){
+  var total = req.body.cart.reduce((a,line)=>{
+    return a + (line.quantity * catalogue[line.product.id])
+  }, 0)
+
+})
+
+app.get("/api/catalogue", function(req, res){
+  res.json(catalogue)
+}) 
+
+// default is to render home page
 app.get('/', function (req, res) {
   res.sendFile(path.join(public_path, 'index.html'));
 })
