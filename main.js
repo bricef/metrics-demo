5f3e5326fe59a86a72a68dd84f11045af69e06e7
Brice Fernandes <brice@weave.works>
2017-09-25 10:28:52 +0100

    Better order logging message

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ app.use('/public', express.static(public_path))
 
 app.post("/api/purchase", function(req, res){
   var order = _.reduce(req.body, (order,item,line)=>{
-    console.log(catalogue[item.id].price, item.count);
     order.count += item.count;
     order.total += (item.count * catalogue[item.id].price);
+    console.log("Order made: ", order);
     return order;
   }, {
     status: "OK",
