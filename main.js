6b79a8d598ff826127049ecc1f75184eff8c53f6
Brice Fernandes <brice@weave.works>
2018-04-10 14:36:57 +0100

    move logging to end of order

--- main.js
+++ main.js
@@ -36,14 +36,13 @@ app.post("/mighty-fine/api/purchase", function(req, res){
   var order = _.reduce(req.body, (order,item,line)=>{
     order.count += item.count;
     order.total += (item.count * catalogue[item.id].price);
-    console.log("Order made: ", order);
     return order;
   }, {
     status: "OK",
     count: 0,
     total: 0,
   })
-  console.log(`Sale of ${order.total} made`)
+  console.log(`Sale of ${order.total} made`);
   sales_counter.inc(order.total);
   res.json(order)
 })
@@ -63,13 +62,13 @@ const unsubscribe_count = new prometheus.Counter({name:'unsubscribe_count', help
 app.post("/mighty-fine/api/subscribe", function(req,res){
   var sub_request = req.body; // ignore the body
   subscribe_count.inc(1)
-  console.log(`Customer subscribed`)
+  console.log(`Customer subscribed`);
   res.json({"subscribed": true, data: sub_request})
 })
 app.post("/mighty-fine/api/unsubscribe", function(req,res){
   var sub_request = req.body; // ignore the body
   unsubscribe_count.inc(1)
-  console.log(`Customer unsubscribed`)
+  console.log(`Customer unsubscribed`);
   res.json({"subscribed": false, data: sub_request})
 })
 
